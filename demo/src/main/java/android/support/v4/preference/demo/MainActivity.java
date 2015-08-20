b6d33b7a184e0a07c373a5419aaf56eacace81e5
shkschneider <shk.schneider@gmail.com>
2015-08-20 17:21:26 +0200

    Completed demo application

--- demo/src/main/java/android/support/v4/preference/demo/MainActivity.java
+++ demo/src/main/java/android/support/v4/preference/demo/MainActivity.java
@@ -1,16 +1,36 @@
 package android.support.v4.preference.demo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(getResources().getDrawable(R.drawable.ic_launcher, null));
+        toolbar.setTitle(getResources().getString(R.string.app_label));
+        toolbar.setSubtitle("Demo");
+        toolbar.inflateMenu(R.menu.main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(final MenuItem item) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/shkschneider/android_PreferenceFragmentCompat")));
+                return true;
+            }
+
+        });
 
         getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new MainPreferenceFragment())
+                .replace(R.id.frameLayout, new MainPreferenceFragment())
                 .commit();
     }
 
