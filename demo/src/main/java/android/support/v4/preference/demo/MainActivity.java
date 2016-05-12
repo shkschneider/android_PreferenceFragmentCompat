f0551ed4f0ded50b25d5c9c79718d7dc19fac49b
shkschneider <shk.schneider@gmail.com>
2016-05-12 14:41:57 +0200

    demo: (support) Toolbar fix

--- demo/src/main/java/android/support/v4/preference/demo/MainActivity.java
+++ demo/src/main/java/android/support/v4/preference/demo/MainActivity.java
@@ -1,11 +1,13 @@
 package android.support.v4.preference.demo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 public class MainActivity extends FragmentActivity {
 
@@ -15,7 +17,8 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(getResources().getDrawable(R.drawable.ic_launcher, null));
+        toolbar.setBackgroundColor(Color.parseColor("#2ecc71"));
+        toolbar.setLogo(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_launcher));
         toolbar.setTitle("Demo");
         toolbar.setSubtitle(getResources().getString(R.string.app_label));
         toolbar.inflateMenu(R.menu.main);
